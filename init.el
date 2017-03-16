c336a7fc254c7081ecbe7ca8173f656602635a58
Denis Tranchemontagne <denis.tranchemontagne@redlinetrading.com>
2017-03-16 07:40:26 -0400

    Convert to org mode and ivy

---
+++ init.el
@@ -0,0 +1,17 @@
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
+
+(require 'ob-tangle)
+;;(org-babel-load-file
+;; (expand-file-name "emacs-init.org"
+;;		   user-emacs-directory))
+
+(org-babel-load-file
+ (expand-file-name "emacs-ivy-init.org"
+		   user-emacs-directory))
