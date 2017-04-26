504d0a5078f0268e4c07eb93b82366e718a32213
Denis Tranchemontagne <denis.tranchemontagne@redlinetrading.com>
2017-04-26 10:46:30 -0400

    Some more tweaks

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (org-bullets counsel flx ivy ag helm-cscope xcscope projectile helm-helm-commands helm use-package auto-complete))))
+    (pdf-tools markdown-preview-eww markdown-mode counsel-projectile org-bullets counsel flx ivy ag helm-cscope xcscope projectile helm-helm-commands helm use-package auto-complete))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
