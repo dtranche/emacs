81208c51584090b6ee93de1e5f3afff61b77eb2c
Denis Tranchemontagne <denis.tranchemontagne@redlinetrading.com>
2019-08-02 16:00:51 -0400

    More improvements

--- custom.el
+++ custom.el
@@ -4,11 +4,18 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (pdf-tools markdown-preview-eww markdown-mode counsel-projectile org-bullets counsel flx ivy ag helm-cscope xcscope projectile helm-helm-commands helm use-package auto-complete))))
+	 (quote
+		(which-key gopher ggtags nlinum-relative ivy-hydra doom-modeline use-package pdf-tools org-bullets markdown-preview-eww markdown-mode magit hydra helm-helm-commands helm-cscope flx dracula-theme dired-sidebar counsel-projectile color-theme-sanityinc-solarized auto-complete alect-themes ag ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :background "unspecified-bg" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 1 :width normal :foundry "default" :family "default"))))
+ '(ac-completion-face ((t (:foreground "dark blue" :underline t))))
+ '(font-lock-comment-face ((t (:foreground "red4"))))
+ '(font-lock-keyword-face ((t (:foreground "dark blue"))))
+ '(font-lock-variable-name-face ((t (:foreground "navy" :weight light))))
+ '(magit-diff-context-highlight ((t (:background "grey95" :foreground "slate gray"))))
+ '(warning ((t (:foreground "Yellow4" :weight bold))))
+ '(widget-field ((t (:inherit nil :foreground "brightwhite")))))
