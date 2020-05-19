32f5452f4812801cab60b397507ada4e8a20f78d
Denis Tranchemontagne <denis.tranchemontagne@redlinetrading.com>
2020-05-19 15:50:51 -0400

    Mostly just formatting
    * Added some new makefile strings
    * Added a function to get the buffers file name
    * Added a function to push a mark to ring buffer - not sure it is worthwhile
    * Turn off garbage collection at start up

--- init.el
+++ init.el
@@ -7,10 +7,30 @@
 	     '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 0.6)
+
+;; Keep a ref to the actual file-name-handler
+(defvar default-file-name-handler-alist file-name-handler-alist)
+
+;; Set the file-name-handler to nil (because regexing is cpu intensive)
+(setq file-name-handler-alist nil)
+
+;; Reset file-name-handler-alist after initialization
+(add-hook 'emacs-startup-hook
+  (lambda ()
+    (setq gc-cons-threshold 16777216
+          gc-cons-percentage 0.1
+          file-name-handler-alist default-file-name-handler-alist)))
+
 (require 'ob-tangle)
 ;;(org-babel-load-file
 ;; (expand-file-name "emacs-init.org"
 ;;		   user-emacs-directory))
+
+(use-package esup
+	:ensure t
+	:commands (esup))
 
 (org-babel-load-file
  (expand-file-name "emacs-ivy-init.org"
